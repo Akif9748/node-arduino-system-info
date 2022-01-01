401fdf21f722380230567db15a01c51a5f06e40c
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-01 21:17:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const SerialPort = require('serialport');
 const os = require('os-utils');
 
-//Your com port and baudrate
+//Your com port and baudrate: (BaudRate is defined in Arduino Code, 9600.)
 const port = new SerialPort('COM3', { baudRate: 9600 });
 
-// Read the port data
+// Port ready message
 port.on("open", () => {
   console.log('Serial is ready!');
 });
 
 
 setInterval(() => {
-  const total = os.totalmem().toFixed(0);
-  const used = (os.totalmem() - os.freemem()).toFixed(0);
-  const ratio = 100 * ((used / total).toFixed(2));
-  os.cpuUsage(usage => {
-
-    port.write(`${used}-${ratio}-${usage.toFixed(2) * 100}`, function (err) {
-      if (err) {
-        console.log('error writing');
-        console.error(err);
-      };
-
+  const total = os.totalmem().toFixed(0);//Total memory on your system
+  const used = (os.totalmem() - os.freemem()).toFixed(0);//Used memory on your system
+  const ratio = 100 * ((used / total).toFixed(2));//Used memory / Total memory
+  os.cpuUsage(usage => {//CPU USAGE
+     /*
+      A format for sending datas to Arduino.
+      UsedRAM-UsedRAMRatio-CPUUsage
+      For example:
+      2824-69-10
+     */
+    port.write(`${used}-${ratio}-${usage.toFixed(2) * 100}`, (err) => {
+      if (err) console.error(err);
     });
 
   });
 
 
-}, 1000);//Per 1000 ms
+}, 1000);//Refreshing per one second
