46ff00121ee55ccf70e3e88c9b6b7ea96efc56a9
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-02-25 20:16:39 +0300

    Update systeminfo.ino

--- systeminfo/systeminfo.ino
+++ systeminfo/systeminfo.ino
@@ -1,57 +1,57 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+
+//LCD pins:
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-char *strings[12];   // For datas
-char *ptr = NULL;    //
-String kol_Str = ""; //
+// String max size: 65536-100-100
+const int size = 14;
 
-String lnk_1 = "";
-String lnk_2 = ""; // For strings
-String lnk_3 = "";
+// For datas:
+String ram , ramUsage, cpu;
+char *strings[size],*ptr = NULL; 
 
 void setup()
 {
-  //LCD size
+  // LCD size
   lcd.begin(16, 2);
-  //Start a serial communication
+  // Start a serial communication
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if (Serial.available())
-  {
-    while (Serial.available() > 0)
+  //While getting data:
+  while (Serial.available() > 1)
+  { 
+    //Read serial
+    String data = Serial.readString();
+    char values[size];
+    
+    data.toCharArray(values, size); // String => Char
+    ptr = strtok(values, "-"); // Split "-"
+
+    int index = 0;
+    while (ptr != NULL)
     {
-      //  Read serial
-      kol_Str = Serial.readString();
-      int index = 0;
-      char kolyedek[13];
-      String kol_Str_yedek;
-      kol_Str.toCharArray(kolyedek, 13); // String => Char
-
-      ptr = strtok(kolyedek, "-"); // Split "-"
-      while (ptr != NULL)
-      {
-        strings[index] = ptr; // Save to Strings
-        index++;
-        ptr = strtok(NULL, "-"); //Split "-"
-      }
-
-      lnk_1 = strings[0];
-      lnk_2 = strings[1];
-      lnk_3 = strings[2];
-      //Clear LCD
-      lcd.clear();
-      //Write to LCD
-      lcd.setCursor(0, 0);
-      lcd.print("RAM: " + lnk_1 + " %" + lnk_2);
-      lcd.setCursor(0, 1);
-      lcd.print("CPU: %" + lnk_3);
+      strings[index] = ptr; // Save to Strings
+      index++;
+      ptr = strtok(NULL, "-"); // Split "-"
     }
+
+    // Values:
+    ram = strings[0];
+    ramUsage = strings[1];
+    cpu = strings[2];
+    
+    // Clear LCD
+    lcd.clear();
+
+    // Write to LCD
+    lcd.setCursor(0, 0);
+    lcd.print("RAM: " + ram + " %" + ramUsage);
+    lcd.setCursor(0, 1);
+    lcd.print("CPU: %" + cpu);
   }
 }
